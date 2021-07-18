cb15e0a3cc92e5024277d00e9156100c7e70d5f3
沃亚男 <woyanan@atlasv.com>
2021-07-19 06:33:58 +0800

    chore: 删除VideoDrawer

--- meishe/src/main/java/com/atlasv/android/meishe/fbo/DefGLSurfaceView.kt
+++ meishe/src/main/java/com/atlasv/android/meishe/fbo/DefGLSurfaceView.kt
@@ -1,10 +1,8 @@
 package com.atlasv.android.meishe.fbo
 
 import android.content.Context
-import android.graphics.PointF
 import android.opengl.GLSurfaceView
 import android.util.AttributeSet
-import android.view.MotionEvent
 
 
 /**
@@ -16,31 +14,5 @@ import android.view.MotionEvent
 class DefGLSurfaceView : GLSurfaceView {
 
     constructor(context: Context) : super(context)
-
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
-
-    private var mPrePoint = PointF()
-
-    private var mDrawer: VideoDrawer? = null
-
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                mPrePoint.x = event.x
-                mPrePoint.y = event.y
-            }
-            MotionEvent.ACTION_MOVE -> {
-                val dx = (event.x - mPrePoint.x) / width
-                val dy = (event.y - mPrePoint.y) / height
-                mDrawer?.translate(dx, dy)
-                mPrePoint.x = event.x
-                mPrePoint.y = event.y
-            }
-        }
-        return true
-    }
-
-    fun addDrawer(drawer: VideoDrawer) {
-        mDrawer = drawer
-    }
 }
